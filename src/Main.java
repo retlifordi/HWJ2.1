a8395c8a6c3c5a81d563a1347ef2de937191d0de
Anastasiia Kosterkina <nastya.kosterkina@mail.ru>
2023-10-13 20:08:40 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 13767; // Обьявление переменной для суммарной стоимость за билет
+        int bmiles;//  Параметр для записи бонусных миль
+
+        bmiles = amount / 20;// расчет бонусных миль
+        System.out.println("Количество бонусных миль: "+ bmiles);
+
+    }
+}
